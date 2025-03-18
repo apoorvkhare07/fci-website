85a396b5b3937795cac3407ae60a4a173d20d609
Apoorv <apoorvkhare007@gmail.com>
2025-03-18 21:23:07 +0530

    gitignore and next config changes

--- packages/landing/next.config.js
+++ packages/landing/next.config.js
@@ -1,10 +1,15 @@
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-  // distDir: '../../dist/functions/next'
   images: {
     domains: ['pbs.twimg.com'],
   },
+  output: 'export',
+  trailingSlash: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  distDir: 'dist',
 };
 
 module.exports = withPlugins([], nextConfig);
